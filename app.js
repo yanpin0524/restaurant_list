c460bff0a814beee3051a25743ba9b2cf85e0268
Leo <a131234568@gmail.com>
2022-02-26 16:54:56 +0800

    set view engine

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const exphbs = require('express-handlebars')
 const port = 3000
 const restaurant_list = require('/restaurant.json')
 
+app.engine('handlebars', exphbs({ defaultLayout: main }))
+app.set('view engine', 'handlebars')
+
