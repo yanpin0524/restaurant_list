99ce860f7676f5615444b954313f11db93780dfe
Leo <a131234568@gmail.com>
2022-02-26 17:10:10 +0800

    初始路由設定

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
-const restaurant_list = require('/restaurant.json')
+const restaurant_list = require('./restaurant.json')
 
-app.engine('handlebars', exphbs({ defaultLayout: main }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.get('/', (req, res) => {
+  res.send('<h1> This is a web app for express. </h1>')
+})
+
+app.listen(port, () => {
+  console.log(`http:/localhost:${port}`)
+})
