738a8b4807b379d522c6697976b577c9b4764b12
Leo <a131234568@gmail.com>
2022-04-03 22:36:27 +0800

    新增 facebook 登入功能

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 
 const { authenticator } = require('../middleware/auth')
 
 router.use('/restaurant', authenticator, restaurant)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
