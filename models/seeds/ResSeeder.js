a320a74eaf55fb2c1853e5591d191295b65028ba
Leo <a131234568@gmail.com>
2022-04-04 02:13:47 +0800

    修改種子資料

--- models/seeds/ResSeeder.js
+++ models/seeds/ResSeeder.js
@@ -1,20 +1,52 @@
+const bcrypt = require('bcryptjs')
 const db = require('../../config/mongoose')
 const Restaurants = require('../restaurant')
-const restaurantList = require('../restaurant.json')
+const User = require('../user')
+const restaurantList = require('./restaurant.json')
+const seedUser = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    have: [0, 1, 2]
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+    have: [3, 4, 5]
+  }
+]
 
 db.once('open', () => {
-  restaurantList.results.forEach((item) => {
-    Restaurants.create({
-      name: item.name,
-      name_en: item.name_en,
-      category: item.category,
-      image: item.image,
-      location: item.location,
-      phone: item.phone,
-      google_map: item.google_map,
-      rating: item.rating,
-      description: item.description
-    })
-  })
-  console.log('種子資料 載入結束')
+  return Promise.all(Array.from(seedUser, (seedUser) => {
+    User.findOne({ email: seedUser.email })
+      .then((user) => {
+        if (user) return user
+
+        return bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(seedUser.password, salt))
+          .then(hash => User.create({
+            name: seedUser.name,
+            email: seedUser.email,
+            password: hash
+          }))
+      })
+      .then(user => {
+        const userId = user._id
+        return Promise.all(Array.from(restaurantList.results, (item, index) => {
+          if (seedUser.have.includes(index)) {
+            const addItem = item
+            addItem.userId = userId
+            Restaurants.create(addItem)
+          }
+        }))
+      })
+  }))
 })
+  .then(() => {
+    console.log('種子資料 載入結束')
+    // process.exit() 啟動之後就無法載入種子資料了，搞不太懂Promise的原理。
+  })
+  .catch(err => console.log(err))
